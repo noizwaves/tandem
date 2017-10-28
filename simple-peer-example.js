d13f5f59f96173ed1bd4e93dd040eaedce32cd25
Adam Neumann <noizwaves@users.noreply.github.com>
2017-10-28 16:29:27 -0600

    Show host/join buttons when app start

--- simple-peer-example.js
+++ simple-peer-example.js
@@ -1,26 +1,55 @@
 var Peer = require('simple-peer')
-var p = new Peer({ initiator: false, trickle: false })
 
-p.on('error', function (err) {
-    console.log('error', err)
-})
+document.querySelector("#host").addEventListener('click', function (ev) {
+    ev.preventDefault();
 
-p.on('signal', function (data) {
-    console.log('SIGNAL', JSON.stringify(data))
-    document.querySelector('#outgoing').textContent = JSON.stringify(data)
-})
+    show("#connect-form");
+    hide("#buttons");
 
-document.querySelector('form').addEventListener('submit', function (ev) {
-    ev.preventDefault()
-    p.signal(JSON.parse(document.querySelector('#incoming').value))
-})
+    establishConnection(true);
+});
 
-p.on('connect', function () {
-    const chunk = 'whatever' + Math.random();
-    p.send(chunk)
-    document.querySelector('#data').textContent += "Sending: " + chunk + ";\n";
-})
+document.querySelector("#join").addEventListener('click', function (ev) {
+    ev.preventDefault();
 
-p.on('data', function (data) {
-    document.querySelector('#data').textContent += "Received: " + data + ";\n";
-})
+    show("#connect-form");
+    hide("#buttons");
+
+    establishConnection(false);
+});
+
+function show(selector) {
+    document.querySelector(selector).classList.remove('hidden');
+}
+
+function hide(selector) {
+    document.querySelector(selector).classList.add('hidden');
+}
+
+function establishConnection(initiator) {
+    var p = new Peer({initiator: initiator, trickle: false})
+
+    p.on('error', function (err) {
+        console.log('error', err)
+    })
+
+    p.on('signal', function (data) {
+        console.log('SIGNAL', JSON.stringify(data))
+        document.querySelector('#outgoing').textContent = JSON.stringify(data)
+    })
+
+    document.querySelector('form').addEventListener('submit', function (ev) {
+        ev.preventDefault()
+        p.signal(JSON.parse(document.querySelector('#incoming').value))
+    })
+
+    p.on('connect', function () {
+        const chunk = 'whatever' + Math.random();
+        p.send(chunk)
+        document.querySelector('#data').textContent += "Sending: " + chunk + ";\n";
+    })
+
+    p.on('data', function (data) {
+        document.querySelector('#data').textContent += "Received: " + data + ";\n";
+    })
+}
