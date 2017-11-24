bee6c55752d285fd0613358e8ecc763b6f0936cc
Adam Neumann <noizwaves@users.noreply.github.com>
2017-11-23 21:54:35 -0700

    Split reception into modules

---
+++ reception/src/Message.elm
@@ -0,0 +1,18 @@
+module Message exposing (..)
+
+import Model exposing (NameInformation)
+
+type Msg
+  = NameChanged String
+
+  | UpdateProcessTrust Bool
+
+  | ReceiveApiMessage String
+
+  | HostSession NameInformation
+  | JoinSession NameInformation
+
+  | ReceiveOfferFromDC String
+  | ReceiveAnswerFromDC String
+
+  | ConnectionStateChanged Bool
