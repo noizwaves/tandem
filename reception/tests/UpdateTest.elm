8af5d5125f6131486fe4360f7431ae75f9b805f3
Adam Neumann <noizwaves@users.noreply.github.com>
2018-01-17 12:17:15 -0700

    When a user disconnects, set the previous name information as the known current information as the disconnect event arrives AFTER the name information change

---
+++ reception/tests/UpdateTest.elm
@@ -0,0 +1,52 @@
+module UpdateTest exposing (..)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+import Debounce
+import Update exposing (update)
+import Message exposing (Msg(..))
+import Model exposing (Model, NameInformation, AppUpdateAvailability(..), ValidatedName(..), ConnectionIntent(..), ProcessTrustLevel(..), ConnectivityLevel(..), PreConnectionIntent(..))
+
+
+suite : Test
+suite =
+  describe "Update module"
+    [ describe "update"
+      [ connectionStateChangedSuite ]
+    ]
+
+standardModel : Model
+standardModel =
+  { appUpdates = UpdateStatusUnknown
+  , name = ValidName "foo"
+  , nameDebouncer = Debounce.init
+  , throttledName = ValidName "foo"
+  , intent = (Browsing Nothing)
+  , trust = TrustUnknown
+  , connectivity = Online
+  }
+
+standardInfo : NameInformation
+standardInfo =
+  { canJoin = True
+  , canHost = True
+  , iceServers = [ ]
+  }
+
+connectionStateChangedSuite : Test
+connectionStateChangedSuite =
+  describe "handling ConnectionStateChanged"
+    [ test "disconnected while hosting" <|
+      \_ ->
+        { standardModel | intent = Connected PreviouslyHosting standardInfo }
+        |> update (ConnectionStateChanged False)
+        |> Tuple.first
+        |> Expect.equal { standardModel | intent = Browsing (Just standardInfo) }
+    , test "disconnected while joined" <|
+      \_ ->
+        { standardModel | intent = Connected PreviouslyJoining standardInfo }
+        |> update (ConnectionStateChanged False)
+        |> Tuple.first
+        |> Expect.equal { standardModel | intent = Browsing (Just standardInfo) }
+    ]
